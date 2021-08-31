b0660dbfbd2f2cd90d8f9d8dfb2d656400a7c6ea
AA406 <aparedes.aedo@gmail.com>
2021-08-31 05:43:40 -0400

    cambios

--- WARF/Assets/Scripts/Movimiento2.cs
+++ WARF/Assets/Scripts/Movimiento2.cs
@@ -12,67 +12,63 @@ public class Movimiento2 : MonoBehaviour
     public GameObject ojoAbierto;
 
     public Movimiento movimiento;
-    public Movimiento2 mv;
-    public Patrulla P;
+    //public Movimiento2 mv;
 
-    public NavMeshAgent Humano;
-    public NavMeshAgent Perro;
 
-    public Rigidbody rb;
+
+    public Rigidbody2D rb;
     public AudioSource caminata;
+
+    public float speed = 4f;
+
     private void Update()
     {
-        MoveInDirectionOfInput();
+
+
 
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            mv.enabled = false;
-
-            movimiento.enabled = true;
 
 
 
-            P.enabled = true;
+            //mv.enabled = true;
 
-            Humano.enabled = false;
+            movimiento.enabled = false;
 
-            Perro.enabled = true;
+
+
+
+
+
+
+
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Humano.enabled = false;
+
+
 
 
         }
 
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            Humano.enabled = true;
+
 
 
 
         }
-    }
-    public void MoveInDirectionOfInput()
-    {
-        if (!noPuedeMoverse)
-        {
-            Vector3 dir = Vector3.zero;
 
-            dir.x = Input.GetAxis("Horizontal");
-            dir.z = Input.GetAxis("Vertical");
-            //AudioManager.obj.playWalk();
+        Vector3 mov = new Vector3(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical"),
+            0
 
-            Vector3 camDirection = Camera.main.transform.rotation * dir; //This takes all 3 axes (good for something flying in 3d space)    
-            Vector3 targetDirection = new Vector3(camDirection.x, 0, camDirection.z); //This line removes the "space ship" 3D flying effect. We take the cam direction but remove the y axis value
+            );
 
-           
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + mov, speed * Time.deltaTime);
 
-            rb.velocity = targetDirection.normalized * VelocidadMovimiento;     //normalized prevents char moving faster than it should with diagonal input
-        }
 
     }
-
-   
 }
